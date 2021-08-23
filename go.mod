9fcbc1964ff4842bf7e6e80a865027b55e5ca816
Richard Snider <richardnsnider@gmail.com>
2021-08-23 20:05:32 +0000

    add byte examples

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/richardsnider/buffer-examples
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+)
+
+func main() {
+	sliceOfBytes := []byte{1, 2, 3, 4, 'a', 'b', 'c', 'd'}
+
+	sliceOfBytes = createSliceOfBytesFromBuffer()
+
+	for index, byteValue := range sliceOfBytes {
+		indexString := fmt.Sprint(index)
+		log.Println("Value at " + indexString + ": " + string(byteValue))
+	}
+}
+
+func createSliceOfBytesFromBuffer() []byte {
+	var bytesBuffer bytes.Buffer
+	bytesBuffer.WriteByte(42)
+	bytesBuffer.WriteByte(13)
+	bytesBuffer.WriteByte('a')  // ascii # 97
+	bytesBuffer.WriteByte('\n') // ascii # 13
+	bytesBuffer.WriteByte(255)  // Largest byte value (hex: 0xFF, octal: 077, binary: 0b11111111, "ÿ")
+	// bytesBuffer.WriteByte(256) // Invalid
+	// bytesBuffer.WriteByte(666) // Invalid
+	bytesBuffer.WriteByte(0xC4)       // (13*16)+4 == 192+4 == 196 == "Ä"
+	bytesBuffer.WriteByte(007)        // 0+7 == 7
+	bytesBuffer.WriteByte(017)        // 8+7 == 15
+	bytesBuffer.WriteByte(0b11000101) // 128+64+0+0+0+4+0+1 == 197 == "Å" == 0xC5
+
+	var bytesLiteral = []byte{43, 'b', 14, 0xC5}
+	bytesBuffer.WriteByte(bytesLiteral[0])
+	bytesBuffer.Write(bytesLiteral)
+
+	return bytesBuffer.Bytes()
+}
